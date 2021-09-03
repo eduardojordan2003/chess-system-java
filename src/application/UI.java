bb34b1a7ec8e478551e54ae309d7f5eeb7b51932
eduardojordan2003 <eduardojordan2003@hotmail.com>
2021-09-03 15:47:14 -0400

    Movie Pieces

--- src/application/UI.java
+++ src/application/UI.java
@@ -1,9 +1,54 @@
 package application;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 import chess.ChessPiece;
+import chess.ChessPosition;
+import chess.Color;
 
 public class UI {
 
+	public static final String ANSI_RESET = "\u001B[0m";
+	public static final String ANSI_BLACK = "\u001B[30m";
+	public static final String ANSI_RED = "\u001B[31m";
+	public static final String ANSI_GREEN = "\u001B[32m";
+	public static final String ANSI_YELLOW = "\u001B[33m";
+	public static final String ANSI_BLUE = "\u001B[34m";
+	public static final String ANSI_PURPLE = "\u001B[35m";
+	public static final String ANSI_CYAN = "\u001B[36m";
+	public static final String ANSI_WHITE = "\u001B[37m";
+
+	public static final String ANSI_BLACK_BACKGROUND = "\u001B[40m";
+	public static final String ANSI_RED_BACKGROUND = "\u001B[41m";
+	public static final String ANSI_GREEN_BACKGROUND = "\u001B[42m";
+	public static final String ANSI_YELLOW_BACKGROUND = "\u001B[43m";
+	public static final String ANSI_BLUE_BACKGROUND = "\u001B[44m";
+	public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
+	public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
+	public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+	
+	
+	public static void clearScreen() {
+		System.out.print("\033[H\033]2J");
+		System.out.flush();
+	}
+	
+	public static ChessPosition readChessPosition(Scanner sc) {
+		
+		try {
+			String s = sc.nextLine();
+			char column = s.charAt(0);
+			int row = Integer.parseInt(s.substring(1));
+			return new ChessPosition(column, row);
+		}
+		catch (RuntimeException e){
+			throw new InputMismatchException("Erro ao ler ChessPosition. Valores valido são entre a1 até h8");
+		}
+		
+	}
+	
+	
 	public static void printBoard(ChessPiece[][] pieces) {
 	
 		for (int i=0;i<pieces.length;i++) {
@@ -17,15 +62,22 @@ public class UI {
 	
 	}
 	
-	public static void printPiece(ChessPiece piece) {
-		
-		if (piece == null) {
-			System.out.print("-");
-		}
-		else {
-			System.out.print(piece);
-		}
-		System.out.print(" ");
+	private static void printPiece(ChessPiece piece /*, boolean background = false*/) {
+		/*if (background) {
+			System.out.print(ANSI_BLUE_BACKGROUND);
+		}*/
+    	if (piece == null) {
+            System.out.print("-" + ANSI_RESET);
+        }
+        else {
+            if (piece.getColor() == Color.WHITE) {
+                System.out.print(ANSI_WHITE + piece + ANSI_RESET);
+            }
+            else {
+                System.out.print(ANSI_YELLOW + piece + ANSI_RESET);
+            }
+        }
+        System.out.print(" ");
 	}
 	
 }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,8 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Piece;
+import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
@@ -26,14 +28,69 @@ public class ChessMatch {
 		
 	}
 	
-	private void PlaceNewPiace(char column, int row, ChessPiece piece) {
+	public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
+		Position source = sourcePosition.toPosition();
+		Position target = targetPosition.toPosition();
+		validateSourcePosition(source);
+		Piece captoredPiece = makeMove(source, target);
+		return (ChessPiece)captoredPiece;
+	}
+	
+	private Piece makeMove(Position source, Position target) {
+		Piece p = board.removePiece(source);
+		Piece capturedPiece = board.removePiece(source);
+		board.PlacePiece(p, target);
+		return capturedPiece;
+				
+		
+	}
+	
+	private void validateSourcePosition(Position position) {
+		if (!board.thereIsAsPiece(position)) {
+			throw new ChessException("Não existe peça na posição de origem");
+		}
+	}
+	
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
 		board.PlacePiece(piece,  new ChessPosition(column, row).toPosition());
 	}
 	
 	private void initialSetup() {
-		PlaceNewPiace('b',6, new Rook(board, Color.WHITE));
-		PlaceNewPiace('e',8, new King(board, Color.BLACK));
-		PlaceNewPiace('e',1, new King(board, Color.WHITE));
+	
+		
+		placeNewPiece('a', 1, new Rook(board, Color.WHITE));
+        //placeNewPiece('b', 1, new Knight(board, Color.WHITE));
+        //placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
+        //placeNewPiece('d', 1, new Queen(board, Color.WHITE));
+        placeNewPiece('e', 1, new King(board, Color.WHITE));
+        //placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
+        //placeNewPiece('g', 1, new Knight(board, Color.WHITE));
+        placeNewPiece('h', 1, new Rook(board, Color.WHITE));
+        //placeNewPiece('a', 2, new Pawn(board, Color.WHITE, this));
+        //placeNewPiece('b', 2, new Pawn(board, Color.WHITE, this));
+        //placeNewPiece('c', 2, new Pawn(board, Color.WHITE, this));
+        //placeNewPiece('d', 2, new Pawn(board, Color.WHITE, this));
+        //placeNewPiece('e', 2, new Pawn(board, Color.WHITE, this));
+        //placeNewPiece('f', 2, new Pawn(board, Color.WHITE, this));
+        //placeNewPiece('g', 2, new Pawn(board, Color.WHITE, this));
+        //placeNewPiece('h', 2, new Pawn(board, Color.WHITE, this));
+
+        placeNewPiece('a', 8, new Rook(board, Color.BLACK));
+        //placeNewPiece('b', 8, new Knight(board, Color.BLACK));
+        //placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
+        //placeNewPiece('d', 8, new Queen(board, Color.BLACK));
+        placeNewPiece('e', 8, new King(board, Color.BLACK));
+        //placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
+        //placeNewPiece('g', 8, new Knight(board, Color.BLACK));
+        //placeNewPiece('h', 8, new Rook(board, Color.BLACK));
+        //placeNewPiece('a', 7, new Pawn(board, Color.BLACK, this));
+        //placeNewPiece('b', 7, new Pawn(board, Color.BLACK, this));
+        //placeNewPiece('c', 7, new Pawn(board, Color.BLACK, this));
+        //placeNewPiece('d', 7, new Pawn(board, Color.BLACK, this));
+        //placeNewPiece('e', 7, new Pawn(board, Color.BLACK, this));
+        //placeNewPiece('f', 7, new Pawn(board, Color.BLACK, this));
+        //placeNewPiece('g', 7, new Pawn(board, Color.BLACK, this));
+        //placeNewPiece('h', 7, new Pawn(board, Color.BLACK, this));
 
 	}
 
