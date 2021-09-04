66ee86f587d2483f00372f63e8de29b9a11f7d57
eduardojordan2003 <eduardojordan2003@hotmail.com>
2021-09-03 20:53:13 -0400

    King Move

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -16,9 +17,58 @@ public class King extends ChessPiece {
 		return "K";
 	}
 
+	public boolean canMoves(Position position){
+		ChessPiece p = (ChessPiece)getBoard().piece(position);
+		return p == null || p.getColor() != getColor();
+	
+	}
+	
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+	
+		Position p = new Position(0, 0);
+		//Acima
+		p.setValues(position.getRow() -1, position.getColumn());
+		if(getBoard().positionExists(p) && canMoves(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		//Abaixo
+		p.setValues(position.getRow() +1, position.getColumn());
+		if(getBoard().positionExists(p) && canMoves(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		//Esquerda
+		p.setValues(position.getRow(), position.getColumn() - 1);
+		if(getBoard().positionExists(p) && canMoves(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		//Direita
+		p.setValues(position.getRow(), position.getColumn() +1);
+		if(getBoard().positionExists(p) && canMoves(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		//Diagonal Superior Esquerda
+		p.setValues(position.getRow() -1, position.getColumn() -1);
+		if(getBoard().positionExists(p) && canMoves(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		//Diagonal Superior Direita
+		p.setValues(position.getRow() -1, position.getColumn() +1);
+		if(getBoard().positionExists(p) && canMoves(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		//Diagonal Inferior Esquerda
+		p.setValues(position.getRow() +1, position.getColumn() -1);
+		if(getBoard().positionExists(p) && canMoves(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		//Diagonal Inferior Esquerda
+		p.setValues(position.getRow() +1, position.getColumn() +1);
+		if(getBoard().positionExists(p) && canMoves(p)) {
+			mat[p.getRow()][p.getColumn()] = true;
+		}
+		
 		return mat;
 	}
 }
